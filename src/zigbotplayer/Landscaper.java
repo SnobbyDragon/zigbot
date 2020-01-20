4d53a5dbeac7cb137e50dd2336493d17571b98e2
Chris <chris.kimmel@live.com>
2020-01-19 21:45:21 -0500

    Added basic Landscaper behavior

--- src/zigbotplayer/Landscaper.java
+++ src/zigbotplayer/Landscaper.java
@@ -1,5 +1,6 @@
 package zigbotplayer;
 
+import battlecode.common.Direction;
 import battlecode.common.GameActionException;
 import battlecode.common.GameConstants;
 import battlecode.common.RobotInfo;
@@ -9,15 +10,25 @@ import java.util.Arrays;
 
 public class Landscaper extends RobotPlayer {
 
-    public void landScaperTurn() throws GameActionException {
-        moveAnywhere();
-        
-        // System.out.println("I'M A LANDSCAPER AND I EXIST!");
+    public void landscaperTurn() throws GameActionException {
+    	// Dig dirt, if possible, from an arbitrary direction
+    	for (Direction dir : directions) {
+        	if (rc.isReady() && rc.canDigDirt(dir)) {
+        		System.out.println("Trying to dig in direction " + dir);
+        		rc.digDirt(dir);
+        	}
+        	
+        // Deposit dirt, if possible, in the SOUTH direction
+        if (rc.isReady() && rc.canDepositDirt(Direction.SOUTH)) {
+        	System.out.println("Trying to deposit dirt south");
+        	rc.depositDirt(Direction.SOUTH);
+        }
+    	} 
     }
-
+   
     public void runUnit() throws GameActionException {
         while (true) {
-            landScaperTurn();
+            landscaperTurn();
             endTurn();
         }
     }
